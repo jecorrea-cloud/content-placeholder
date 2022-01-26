9a58a4ec2bc2794d7c30861d0d84a945310a9174
joaquin <jecorrea@protonmail.com>
2022-01-26 18:06:00 -0500

    initial JS

---
+++ script.js
@@ -0,0 +1,13 @@
+// Bring down the HTML elements
+
+const header = document.getElementById("header");
+const title = document.getElementById("title");
+const excerpt = document.getElementById("excerpt");
+const profile_img = document.getElementById("profile_img");
+const name = document.getElementById("name");
+const date = document.getElementById("date");
+
+// Bring down the animations
+
+const animated_bgs = document.querySelectorAll(".animated-bg");
+const animated_bg_texts = document.querySelectorAll(".animated-bg-text");
